d575096d35c5fef63274571f2c1b365d631cd9d4
Ankur Chauhan <ankur@malloc64.com>
2013-01-21 05:06:29 -0800

    Bring back some magic

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require "sinatra/twilio"
 
 require 'logger'
 
+# A hack around multiple routes in Sinatra
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
+
 # Home page and reference
 get '/' do
   @title = "Home"
@@ -15,7 +21,7 @@ logger.level = Logger::DEBUG
 
 # Generic voice call handler, receives calls and replies with pin if one of the owners
 # else redirects to authenticate flow
-respond "/call" do
+get_or_post "/call" do
   logger.debug "Call received from #{params[:From]} with parameters #{params} for pin query"
 
   addSay "Welcome caller."
@@ -30,7 +36,7 @@ respond "/call" do
 end
 
 # SMS Request URL
-respond '/sms' do
+get_or_post '/sms' do
   response = Twilio::TwiML::Response.new
   if caller_pins.has_key? params[:From]
     case params[:Body]
@@ -49,13 +55,13 @@ respond '/sms' do
   response.text
 end
 # Sends back play message
-respond "/allowed_call" do  
+get_or_post "/allowed_call" do  
   addPlay "http://www.dialabc.com/i/cache/dtmfgen/wavpcm8.300/9.wav"
 end
 
 # performs authentication by asking user to key in the pin,
 # it then looks up the pin (if params[:Digits] is set) and redirects to /allowed_call if successful
-respond "/authenticate" do
+get_or_post "/authenticate" do
 
   if caller_pins.has_value? params[:Digits]
     logger.info "[ACCESS GRANTED] to caller #{params[:From]}"    
