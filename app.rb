6debfc2ac204178bdb01b46efa831cb0c66b3ab5
Ankur Chauhan <ankur@malloc64.com>
2013-01-21 04:02:40 -0800

    Fix problems with the script

--- app.rb
+++ app.rb
@@ -1,72 +1,38 @@
 require 'sinatra'
 require "sinatra/twilio"
 
-# A hack around multiple routes in Sinatra
-def get_or_post(path, opts={}, &block)
-  get(path, opts, &block)
-  post(path, opts, &block)
-end
-
 # Home page and reference
 get '/' do
   @title = "Home"
   erb :home
 end
 
-# Voice Request URL
-# get '/voice/?' do
-#   response = Twilio::TwiML::Response.new do |r|
-#     r.Gather :timeout => 10, :method =>'POST', :action => 'http://immense-headland-2964.herokuapp.com/voice' do |g|
-#       g.Say 'Please enter your passcode', :voice => 'woman'
-#     end        
-#   end
-#   response.text
-# end
-
-
-# post '/voice/?' do
-#   response = Twilio::TwiML::Response.new do |r|
-#     r.Play 'http://www.dialabc.com/i/cache/dtmfgen/wavpcm8.300/9.wav', :loop => 5
-#   end
-#   response.text
-# end
-
-
-callers = %w[+15551234567]
-pin     = "1234"
+caller_pins = Hash.new{ '+12139095359' => 161286, '+12069193585' => 170689 }
 
 respond "/call" do
   addSay "Welcome caller."
 
-  if callers.include? params[:From]
-    addRedirect "/allowed_call"
+  if caller_pins.has_key? params[:From]
+    # An authorized user is calling reply with the pins
+    addSay "Your pin is #{caller_pins[params[:From]]}"    
   else
-    addRedirect "/disallowed_call"
+    addRedirect "/authenticate"
   end
 end
 
 respond "/allowed_call" do
-  addPlay "/latest_message.mp3"
-end
-
-respond "/disallowed_call" do
-  gather = Twilio::Gather.new(:action => "/authenticate")
-  gather.addSay "Please enter your PIN now:"
-  append gather
-
-  addSay "You did not enter a pin. Good bye!"
-  addHangup
+  addPlay "http://www.dialabc.com/i/cache/dtmfgen/wavpcm8.300/9.wav"
 end
 
 respond "/authenticate" do
-  if params[:Digits] == pin
+  if caller_pins.has_value? params[:Digits]
     addRedirect "/allowed_call"
   else
-    addRedirect "/disallowed_call"
-  end
-end
+    gather = Twilio::Gather.new(:action => "/authenticate")
+    gather.addSay "Please enter your PIN followed by the pound key:"
+    append gather
 
-get "/latest_message.mp3" do
-  # Assuming we have an ORM...
-  send_file Message.last.path, :stream => true
+    addSay "You did not enter a pin. Good bye!"
+    addHangup
+  end
 end
